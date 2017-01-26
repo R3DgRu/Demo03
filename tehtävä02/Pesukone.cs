95f9fe0a39570a891b693808e09eb953a4661bfc
Otto Savolainen <k2534@student.jamk.fi>
2017-01-26 15:32:30 +0200

    tehtävät 1 ja 2 tehty

---
+++ tehtävä02/Pesukone.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtävä02
+{
+    class Pesukone
+    {
+        public bool On { get; set; }
+        public int Kirjopesu { get; set; }
+        public int Valkopesu { get; set; }
+        public int Linkous { get; set; }
+
+        public void Taytto()
+        {
+            Console.WriteLine("Tayta pesukone");
+        }
+
+        public void Kaynnistys()
+        {
+            Console.WriteLine("Kaynnistys: lori lori, whhrrrrr");
+        }
+
+        public void Tyhjennys()
+        {
+            Console.WriteLine("Tyhjenna pesukone");
+        }
+
+        public void AsetuksetTulostus()
+        {
+            Console.WriteLine("Pesukoneen asetukset");
+            Console.WriteLine("Kirjopesu: {0} C", Kirjopesu );
+            Console.WriteLine("Valkopesu: {0} C", Valkopesu);
+            Console.WriteLine("Linkous: {0} rpm", Linkous);
+        }
+    }
+}
---
+++ tehtävä02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtävä02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pesukone pesukone = new Pesukone();
+            pesukone.Kirjopesu = 0;
+            pesukone.Valkopesu = 40;
+            pesukone.Linkous = 1200;
+
+            pesukone.AsetuksetTulostus();
+            pesukone.Taytto();
+            pesukone.Kaynnistys();
+            pesukone.Tyhjennys();
+        }
+    }
+}
