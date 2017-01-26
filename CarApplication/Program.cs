818e1c209559467fee163f994f322557a9fabfe1
Otto Savolainen <k2534@student.jamk.fi>
2017-01-26 14:28:39 +0200

    autoja

--- CarApplication/Program.cs
+++ CarApplication/Program.cs
@@ -10,6 +10,27 @@ namespace CarApplication
     {
         static void Main(string[] args)
         {
+            // create a new instance from a Car class
+            Car kottero = new Car();
+            kottero.PrintData();
+            kottero.Model = "Fiat Multipla";
+            kottero.Color = "Grey";
+            kottero.Engine = 1.6;
+            kottero.Speed = 10;
+            kottero.FuzzyDices = true;
+            kottero.DoorCount = 5;
+            kottero.Accelerate();
+            kottero.PrintData();
+            kottero.Brake(7);
+            Car.SomeProperty = 56;
+            kottero.PrintData();
+
+            // create another car object
+            string model = "Speedster";
+            Car nascar = new Car(model);
+            nascar.PrintData();
+            nascar.Color = "Black";
+            Console.WriteLine("Color is " + nascar.Color);
         }
     }
 }
