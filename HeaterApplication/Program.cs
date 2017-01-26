b48694c6064d380bcc1e19faadc4502f66c7c4a4
Otto Savolainen <k2534@student.jamk.fi>
2017-01-26 15:06:34 +0200

    tehtävä 1 tehty

---
+++ HeaterApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeaterApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Heater kiuas = new Heater();
+            kiuas.IsOn = true;
+            kiuas.Temperature = 78;
+            kiuas.Humidity = 90;
+            kiuas.ThrowWater();
+            Console.WriteLine("Temperature is " + kiuas.Temperature);
+            Console.WriteLine("Humidity is " + kiuas.Humidity);
+        }
+    }
+}
