e49cc36e2cc23735f81d0b6ec262047ecc7efa85
Otto Savolainen <k2534@student.jamk.fi>
2017-02-01 19:56:17 +0200

    Räpälletty vitos tehtävää

---
+++ tehtava05/Opiskelija.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtava05
+{
+    class Opiskelija
+    {
+        public int Pituus { get; set; } 
+        public int Paino { get; set; }
+        public string Silmienvari { get; set; }
+
+        public void Ohjelmointi()
+        {
+            Console.WriteLine("koodaillaan: public void Ohjelmointi() { Console.WriteLine(trolololol) } jne...");
+        }
+
+        public void Nukkua()
+        {
+            Console.WriteLine("nukkumaan: zzz hgrhhh zzz phyyh zzz...");
+        }
+
+        public void Syoda()
+        {
+            Console.WriteLine("syodaan: mums mums...  ...röyh");
+        }
+    }
+}
---
+++ tehtava05/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtava05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Opiskelija opiskelija = new Opiskelija();
+
+            string[] opiskelija1 = { opiskelija.Silmienvari = "vihr" };
+
+            //opiskelija.Paino = 78;
+            //opiskelija.Pituus = 185;
+            opiskelija.Silmienvari = "sininen";
+        
+
+            opiskelija.Ohjelmointi();
+        }
+    }
+}
